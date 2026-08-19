5a540875a991dc9991b8e395b335ead95d6522c5
seminiariyawansha <ariyawa-im23029@stu.kln.ac.lk>
2026-08-19 10:10:03 +0530

    fix MainActivity template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.example.simpledistancetracker"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
