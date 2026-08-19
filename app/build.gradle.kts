ba7130a4bbe35c5edc2a32e1d515de9eb4947a11
seminiariyawansha <ariyawa-im23029@stu.kln.ac.lk>
2026-08-19 09:29:56 +0530

    Add permission handling, manifest, and gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
